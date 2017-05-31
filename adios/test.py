3ff7e72910f2c0db39b7d16a41f6d150c6b2735e
kevin <liujiezhang@bupt.edu.cn>
2017-05-31 17:44:17 +0800

    main

--- adios/test.py
+++ adios/test.py
@@ -24,73 +24,6 @@ import numpy as np
 import itertools
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-# load data
-print 'loading data.....'
-texts,labels = load_data_and_labels('../docs/CNN/imageText_ml_v5',split_tag='@@@',lbl_text_index=[0,1])
-
-# calcu tf-idf weights
-vectorizer = TfidfVectorizer(min_df=1)
-print 'transfoming tfifd vector .....'
-texts_vec = vectorizer.fit_transform([' '.join(x) for x in texts])
-vecs = []
-print 'deal to xml format text......'
-cnt = 0
-for i in texts_vec:
-    w_ids = i.indices
-    weights = i[:,w_ids].data[::-1]
-    vec_line = ' '.join(['%s:%.4f'%(w_ids[ii],weights[ii]) for ii in range(len(w_ids))])
-    vecs.append(vec_line)
-    cnt += 1
-    if cnt % 2000 == 0:
-        print cnt
-# labels Prepare
-labels = y2list(labels)
-print 'start filter labels....'
-vecs,labels = filter_data(vecs,labels)
-# cate
-lbl_counts = Counter(itertools.chain(*labels))
-# Mapping from index to word
-cate = [x[0] for x in lbl_counts.most_common()]
-cate_id = {v:i for i,v in enumerate(cate)}
-# xml labels
-print 'deal to xml format labels.....'
-xml_lbls = []
-for i,lbls in enumerate(labels):
-    xml_lbls.append(' '.join(['%s:1'%cate_id[lbl] for lbl in lbls]))
-    if i %2000==0:
-        print i
-
-# shuffle
-print 'shuffle and split .....'
-ind = np.arange(len(vecs))
-np.random.shuffle(ind)
-
-# split train and test
-tst_n = int(0.2*len(vecs))
-with open('../docs/CNN/xml_train_x_a','w') as ftx,\
-     open('../docs/CNN/xml_train_y_a','w') as fty:
-     ftx.write('%s %s\n'%(len(vecs) - tst_n,len(vectorizer.vocabulary_)))
-     fty.write('%s %s\n'%(len(vecs) - tst_n,len(cate_id)))
-
-     lbl_temp = np.array(xml_lbls)[ind[tst_n:]]
-     for i,txt in enumerate(np.array(vecs)[ind[tst_n:]]):
-         ftx.write('%s\n'%txt)
-         fty.write('%s\n'%lbl_temp[i])
-         if i%2000 == 0:
-             print i
-
-with open('../docs/CNN/xml_test_x_a','w') as ftx,\
-     open('../docs/CNN/xml_test_y_a','w') as fty:
-     ftx.write('%s %s\n'%(tst_n,len(vectorizer.vocabulary_)))
-     fty.write('%s %s\n'%(tst_n,len(cate_id)))
-
-     lbl_temp = np.array(xml_lbls)[ind[:tst_n]]
-     for i,txt in enumerate(np.array(vecs)[ind[:tst_n]]):
-         ftx.write('%s\n'%txt)
-         fty.write('%s\n'%lbl_temp[i])
-         if i  % 2000 ==0:
-             print i
-
 def y2list(y):
 
     y = [yy[0].strip('\n').split('&') for yy in y]
@@ -143,3 +76,70 @@ def load_data_and_labels(file_path, split_tag='\t', lbl_text_index=[0, 1], is_sh
         np.random.shuffle(ind)
         texts = list(np.array(texts)[ind])
         labels = list(np.array(labels)[ind])
+if __name__ == '__main__':
+    # load data
+    print 'loading data.....'
+    texts,labels = load_data_and_labels('../docs/CNN/imageText_ml_v5',split_tag='@@@',lbl_text_index=[0,1])
+
+    # calcu tf-idf weights
+    vectorizer = TfidfVectorizer(min_df=1)
+    print 'transfoming tfifd vector .....'
+    texts_vec = vectorizer.fit_transform([' '.join(x) for x in texts])
+    vecs = []
+    print 'deal to xml format text......'
+    cnt = 0
+    for i in texts_vec:
+        w_ids = i.indices
+        weights = i[:,w_ids].data[::-1]
+        vec_line = ' '.join(['%s:%.4f'%(w_ids[ii],weights[ii]) for ii in range(len(w_ids))])
+        vecs.append(vec_line)
+        cnt += 1
+        if cnt % 2000 == 0:
+            print cnt
+    # labels Prepare
+    labels = y2list(labels)
+    print 'start filter labels....'
+    vecs,labels = filter_data(vecs,labels)
+    # cate
+    lbl_counts = Counter(itertools.chain(*labels))
+    # Mapping from index to word
+    cate = [x[0] for x in lbl_counts.most_common()]
+    cate_id = {v:i for i,v in enumerate(cate)}
+    # xml labels
+    print 'deal to xml format labels.....'
+    xml_lbls = []
+    for i,lbls in enumerate(labels):
+        xml_lbls.append(' '.join(['%s:1'%cate_id[lbl] for lbl in lbls]))
+        if i %2000==0:
+            print i
+
+    # shuffle
+    print 'shuffle and split .....'
+    ind = np.arange(len(vecs))
+    np.random.shuffle(ind)
+
+    # split train and test
+    tst_n = int(0.2*len(vecs))
+    with open('../docs/CNN/xml_train_x_a','w') as ftx,\
+         open('../docs/CNN/xml_train_y_a','w') as fty:
+         ftx.write('%s %s\n'%(len(vecs) - tst_n,len(vectorizer.vocabulary_)))
+         fty.write('%s %s\n'%(len(vecs) - tst_n,len(cate_id)))
+
+         lbl_temp = np.array(xml_lbls)[ind[tst_n:]]
+         for i,txt in enumerate(np.array(vecs)[ind[tst_n:]]):
+             ftx.write('%s\n'%txt)
+             fty.write('%s\n'%lbl_temp[i])
+             if i%2000 == 0:
+                 print i
+
+    with open('../docs/CNN/xml_test_x_a','w') as ftx,\
+         open('../docs/CNN/xml_test_y_a','w') as fty:
+         ftx.write('%s %s\n'%(tst_n,len(vectorizer.vocabulary_)))
+         fty.write('%s %s\n'%(tst_n,len(cate_id)))
+
+         lbl_temp = np.array(xml_lbls)[ind[:tst_n]]
+         for i,txt in enumerate(np.array(vecs)[ind[:tst_n]]):
+             ftx.write('%s\n'%txt)
+             fty.write('%s\n'%lbl_temp[i])
+             if i  % 2000 ==0:
+                 print i
