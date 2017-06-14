cbf74f372a4b59a9b46580cf08f3a6186b8065f1
Zhang LiuJie(大数据中心/实习生) <liujiezhang@sohu-inc.com>
2017-06-14 10:40:08 +0800

    test cate bug

--- adios/adios_train.py
+++ adios/adios_train.py
@@ -389,6 +389,8 @@ if __name__ == '__main__':
     # add first cate
     trn_labels = y2list_and_g1(trn_labels)
     tst_labels = y2list_and_g1(tst_labels)
+    for i in range(100):
+        print(' '.join(trn_labels[i]))
 
     # filter 其他 and 新闻
     trn_text, trn_labels = filter_data(trn_text, trn_labels)
@@ -398,7 +400,9 @@ if __name__ == '__main__':
     cate_counts = Counter(itertools.chain(*_labels))
     # Mapping from index to word
     cate = [x[0] for x in cate_counts.most_common()]
-
+    for i in range(len(cate)):
+        print(cate[i])
+    exit()
     cate_id = {v: i for i, v in enumerate(cate)}
     Y0 = filter(lambda x: re.search('G1', x), cate)
     Y1 = filter(lambda x: not re.search('G1', x), cate)
