f8ff9ead9b435dde7dd88cce42ddc5dde6fc3daa
Zhang LiuJie(大数据中心/实习生) <liujiezhang@sohu-inc.com>
2017-06-14 13:08:53 +0800

    test no G1

--- adios/adios_train.py
+++ adios/adios_train.py
@@ -282,7 +282,7 @@ def filter_data(x, y):
     res_x, res_y = [], []
     for i, yy in enumerate(y):
         temp_y = filter(lambda lbl: '其他' not in lbl and '新闻' not in lbl, yy)
-        if temp_y and not (len(temp_y) == 1 and 'G1' in temp_y[0]):
+        if len(temp_y) > 1:  # and not (len(temp_y) == 1 and 'G1' in temp_y[0]):
             res_x.append(x[i])
             res_y.append(temp_y)
         if i % 2000 == 0:
@@ -387,8 +387,8 @@ if __name__ == '__main__':
     # id_cate = dict(zip([_id[0] for _id in ids], [cate[0] for cate in cates]))
 
     # add first cate
-    trn_labels = y2list_and_g1(trn_labels)
-    tst_labels = y2list_and_g1(tst_labels)
+    trn_labels = y2list(trn_labels)
+    tst_labels = y2list(tst_labels)
 
     # filter 其他 and 新闻
     trn_text, trn_labels = filter_data(trn_text, trn_labels)
@@ -400,8 +400,8 @@ if __name__ == '__main__':
     cate = [x[0] for x in cate_counts.most_common()]
     cate_id = {v: i for i, v in enumerate(cate)}
 
-    Y0 = filter(lambda x: re.search('G1', x), cate)
-    Y1 = filter(lambda x: not re.search('G1', x), cate)
+    Y1 = filter(lambda x: re.search('-|_', x), cate)
+    Y0 = filter(lambda x: not re.search('-|_', x), cate)
     Y0Y1 = Y0 + Y1
 
     print('Y0 length: %s, Y1 length : %s' % (len(Y0), len(Y1)))
