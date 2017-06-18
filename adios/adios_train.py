951b841aacbf1d97505a80831aac8db42b90b428
Zhang LiuJie(大数据中心/实习生) <liujiezhang@sohu-inc.com>
2017-06-18 18:59:34 +0800

    add pad_wd in vocab

--- adios/adios_train.py
+++ adios/adios_train.py
@@ -385,8 +385,7 @@ if __name__ == '__main__':
             '../docs/CNN/featureMap', lbl_text_index=[1, 0])[0]
     ]
     # add <PAD/>
-    # vocabulary_inv.insert(0, '<PAD/>')
-    vocabulary_inv[0] = '<PAD/>'
+    vocabulary_inv.insert(0, '<PAD/>')
     vocabulary = {v: i for i, v in enumerate(vocabulary_inv)}
     # Load the datasets
     trn_text, trn_labels, tst_text, tst_labels, vocabulary, vocabulary_inv = load_data(
