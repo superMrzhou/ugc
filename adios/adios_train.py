1cad59b5c7cf86592f8f4d1809cb29421bd62814
Zhang LiuJie(大数据中心/实习生) <liujiezhang@sohu-inc.com>
2017-06-18 20:22:30 +0800

    test

--- adios/adios_train.py
+++ adios/adios_train.py
@@ -389,7 +389,7 @@ if __name__ == '__main__':
     vocabulary = {v: i for i, v in enumerate(vocabulary_inv)}
     # Load the datasets
     trn_text, trn_labels, tst_text, tst_labels, vocabulary, vocabulary_inv = load_data(
-        '../docs/CNN/trainString',
+        '../docs/CNN/testString',
         # tst_file='../docs/CNN/testString',
         use_tst=True,
         lbl_text_index=[0, 1],
