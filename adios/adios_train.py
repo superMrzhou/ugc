d97f40b88b520a17baa900312321f65def08365b
Zhang LiuJie(大数据中心/实习生) <liujiezhang@sohu-inc.com>
2017-06-18 17:15:15 +0800

    test alone file

--- adios/adios_train.py
+++ adios/adios_train.py
@@ -365,14 +365,16 @@ if __name__ == '__main__':
     vocabulary_inv = [
         wd[0]
         for wd in load_data_and_labels(
-            '../docs/CNN/dic_ml_v5', lbl_text_index=[1, 0])[0]
+            '../docs/CNN/featureMap', lbl_text_index=[1, 0])[0]
     ]
     # add <PAD/>
-    vocabulary_inv.insert(0, '<PAD/>')
+    # vocabulary_inv.insert(0, '<PAD/>')
+    vocabulary_inv[0] = '<PAD/>'
     vocabulary = {v: i for i, v in enumerate(vocabulary_inv)}
     # Load the datasets
     trn_text, trn_labels, tst_text, tst_labels, vocabulary, vocabulary_inv = load_data(
-        '../docs/CNN/imageText_ml_v5',
+        '../docs/CNN/trainString',
+        tst_file='../docs/CNN/testString',
         use_tst=True,
         lbl_text_index=[0, 1],
         split_tag='@@@',
