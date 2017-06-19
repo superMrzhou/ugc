973a66e295711a2cf058f603cc1deada8a3d5ec7
Zhang LiuJie(大数据中心/实习生) <liujiezhang@sohu-inc.com>
2017-06-19 10:36:26 +0800

    restrict gpu_fraction

--- adios/adios_train.py
+++ adios/adios_train.py
@@ -29,8 +29,8 @@ from utils.callbacks import HammingLoss
 from utils.metrics import f1_measure, hamming_loss, precision_at_k
 from utils.assemble import assemble
 
-# import tensorflow as tf
-# import keras.backend.tensorflow_backend as KTF
+import tensorflow as tf
+import keras.backend.tensorflow_backend as KTF
 
 from utils.data_helper import *
 
@@ -38,17 +38,17 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 
-# def get_session(gpu_fraction=0.3):
-#     '''Assume that you have 6GB of GPU memory and want to allocate ~2GB'''
-#
-#     num_threads = os.environ.get('OMP_NUM_THREADS')
-#     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction)
-#
-#     if num_threads:
-#         return tf.Session(config=tf.ConfigProto(
-#             gpu_options=gpu_options, intra_op_parallelism_threads=num_threads))
-#     else:
-#         return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+def get_session(gpu_fraction=0.3):
+    '''Assume that you have 6GB of GPU memory and want to allocate ~2GB'''
+
+    num_threads = os.environ.get('OMP_NUM_THREADS')
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction)
+
+    if num_threads:
+        return tf.Session(config=tf.ConfigProto(
+            gpu_options=gpu_options, intra_op_parallelism_threads=num_threads))
+    else:
+        return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
 
 def train(train_dataset, valid_dataset, test_dataset, params):
@@ -471,7 +471,7 @@ if __name__ == '__main__':
         'Y0': tst_labels[valid_N:, :nb_labels_Y0],
         'Y1': tst_labels[valid_N:, nb_labels_Y0:]
     }
-    # set gpu_options
-    # KTF.set_session(get_session(gpu_fraction=params['iter']['gpu_fraction']))
+    # set gpu_option
+    KTF.set_session(get_session(gpu_fraction=params['iter']['gpu_fraction']))
     # start train
     train(train_dataset, valid_dataset, test_dataset, params)
