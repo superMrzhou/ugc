3124568db6b948c938d860a3d2d9d3fc9db2ebca
Zhang LiuJie(大数据中心/实习生) <liujiezhang@sohu-inc.com>
2017-06-22 13:52:24 +0800

    decrease threshold

--- adios/utils/models.py
+++ adios/utils/models.py
@@ -49,7 +49,7 @@ class MLC(Model):
         col = F1.argmax(axis=1)
         p_sorted = np.hstack([p_sorted, np.zeros(nb_samples)[:, None]])
         # T = p_sorted[row, col][:,None]
-        T = 0.5 * (p_sorted[row, col] + p_sorted[row, col + 1])[:, None]
+        T = 0.392 * (p_sorted[row, col] + p_sorted[row, col + 1])[:, None]
 
         return T
 
