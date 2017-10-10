d9c73a8aa29b1cd66e2f424ce771f777730cbae1
KillersDeath <KillersDeath@users.noreply.github.com>
2017-10-10 20:22:06 +0800

    constructing HISO models

--- hiso/test.py
+++ hiso/test.py
@@ -1,7 +1,182 @@
-def iterdata():
-    while 1:
-        for i in range(10):
-            yield {'a':i, 'b':i**2}
-it = iterdata()
-aa = [it.next() for i in range(10)]
-print(aa)
+import numpy as np
+import tensorflow as tf
+from keras import backend as K
+from keras.layers import Dense, multiply
+from keras.metrics import binary_accuracy
+from keras.objectives import binary_crossentropy
+
+sess = tf.Session()
+K.set_session(sess)
+
+np.random.seed(1337)  # for reproducibility
+
+input_dim = 32
+
+
+def build_model():
+    inputs = tf.placeholder(tf.float32, shape=[None, input_dim])
+    tf.summary.histogram('inputs', inputs)
+
+    with tf.name_scope('attention_layer'):
+        # ATTENTION PART STARTS HERE
+        with tf.name_scope('weights'):
+            attention_probs = Dense(
+                input_dim, activation='softmax', name='attention_vec')(inputs)
+            variable_summaries(attention_probs)
+        with tf.name_scope('inputs_weighted'):
+            attention_mul = multiply([inputs, attention_probs])
+            variable_summaries(attention_mul)
+            # ATTENTION PART FINISHES HERE
+
+    attention_mul = Dense(64)(attention_mul)
+    with tf.name_scope('predictions'):
+        preds = Dense(1, activation='sigmoid')(attention_mul)
+        tf.summary.histogram('preds', preds)
+    labels = tf.placeholder(tf.float32, shape=[None, 1])
+
+    loss = tf.reduce_mean(binary_crossentropy(labels, preds))
+    tf.summary.scalar('loss', loss)
+    acc_value = tf.reduce_mean(binary_accuracy(labels, preds))
+    tf.summary.scalar('accuracy', acc_value)
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+
+    merged = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter('../docs/train', sess.graph)
+    test_writer = tf.summary.FileWriter('../docs/test')
+    # initializers
+    init_op = tf.global_variables_initializer()
+    sess.run(init_op)
+    # run model
+    with sess.as_default():
+        # feed data to training
+        number_of_training_data = len(outputs)
+        batch_size = 20
+        for epoch in range(10):
+            for start, end in zip(
+                    range(0, number_of_training_data, batch_size),
+                    range(batch_size, number_of_training_data, batch_size)):
+                # _, trn_loss = sess.run(
+                #     [train_step, loss],
+                #     feed_dict={
+                #         inputs: inputs_1[start:end],
+                #         labels: outputs[start:end],
+                #         K.learning_phase(): 1
+                #     })
+                if start % 10 == 0:
+                    summary = sess.run(
+                        merged,
+                        feed_dict={
+                            inputs: inputs_1,
+                            labels: outputs,
+                            K.learning_phase(): 0
+                        })
+                    test_writer.add_summary(summary, start / 10)
+                else:
+                    summary, _ = sess.run(
+                        [merged, train_step],
+                        feed_dict={
+                            inputs: inputs_1[start:end],
+                            labels: outputs[start:end],
+                            K.learning_phase(): 1
+                        })
+                    train_writer.add_summary(summary, start / 10)
+            # print('epoch_step:{}, loss:{}, acc:{}'.format(
+            #     epoch, tst_loss, tst_acc))
+
+
+def variable_summaries(var):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
+
+def get_activations(model, inputs, print_shape_only=False, layer_name=None):
+    # Documentation is available online on Github at the address below.
+    # From: https://github.com/philipperemy/keras-visualize-activations
+    print('----- activations -----')
+    activations = []
+    inp = model.input
+    if layer_name is None:
+        outputs = [layer.output for layer in model.layers]
+    else:
+        outputs = [
+            layer.output for layer in model.layers if layer.name == layer_name
+        ]  # all layer outputs
+    funcs = [
+        K.function([inp] + [K.learning_phase()], [out]) for out in outputs
+    ]  # evaluation functions
+    layer_outputs = [func([inputs, 1.])[0] for func in funcs]
+    for layer_activations in layer_outputs:
+        activations.append(layer_activations)
+        if print_shape_only:
+            print(layer_activations.shape)
+        else:
+            print(layer_activations)
+    return activations
+
+
+def get_data(n, input_dim, attention_column=1):
+    """
+    Data generation. x is purely random except that it's first value equals the target y.
+    In practice, the network should learn that the target = x[attention_column].
+    Therefore, most of its attention should be focused on the value addressed by attention_column.
+    :param n: the number of samples to retrieve.
+    :param input_dim: the number of dimensions of each element in the series.
+    :param attention_column: the column linked to the target. Everything else is purely random.
+    :return: x: model inputs, y: model targets
+    """
+    x = np.random.standard_normal(size=(n, input_dim))
+    y = np.random.randint(low=0, high=2, size=(n, 1))
+    x[:, attention_column] = y[:, 0]
+    return x, y
+
+
+def get_data_recurrent(n, time_steps, input_dim, attention_column=10):
+    """
+    Data generation. x is purely random except that it's first value equals the target y.
+    In practice, the network should learn that the target = x[attention_column].
+    Therefore, most of its attention should be focused on the value addressed by attention_column.
+    :param n: the number of samples to retrieve.
+    :param time_steps: the number of time steps of your series.
+    :param input_dim: the number of dimensions of each element in the series.
+    :param attention_column: the column linked to the target. Everything else is purely random.
+    :return: x: model inputs, y: model targets
+    """
+    x = np.random.standard_normal(size=(n, time_steps, input_dim))
+    y = np.random.randint(low=0, high=2, size=(n, 1))
+    x[:, attention_column, :] = np.tile(y[:], (1, input_dim))
+    return x, y
+
+
+if __name__ == '__main__':
+    N = 10000
+    inputs_1, outputs = get_data(N, input_dim)
+    build_model()
+
+    # m.fit([inputs_1], outputs, epochs=20, batch_size=64, validation_split=0.5)
+    #
+    # testing_inputs_1, testing_outputs = get_data(1, input_dim)
+    #
+    # # Attention vector corresponds to the second matrix.
+    # # The first one is the Inputs output.
+    # attention_vector = get_activations(m, testing_inputs_1,
+    #                                    print_shape_only=True,
+    #                                    layer_name='attention_vec')[0].flatten()
+    # print('attention =', attention_vector)
+    #
+    # # plot part.
+    # import matplotlib.pyplot as plt
+    # import pandas as pd
+    #
+    # pd.DataFrame(attention_vector, columns=['attention (%)']).plot(kind='bar',
+    #                                                                title='Attention Mechanism as '
+    #                                                                      'a function of input'
+    #                                                                      ' dimensions.')
+    # plt.show()
--- hiso/utils/assemble.py
+++ hiso/utils/assemble.py
@@ -1,15 +1,15 @@
 """
 Utility functions for constructing MLC models.
 """
-from keras.layers import Conv1D, Embedding, Flatten, AvgPool1D, MaxPool1D
-from keras.layers import Dense, Dropout, Input, Activation
-from keras.layers import ActivityRegularization, concatenate
+import tensorflow as tf
+from keras.layers import Embedding, Dense, Dropout, concatenate, GRU
+from keras.layers.wrappers import Bidirectional
+from keras.layers import ActivityRegularization
 from keras.layers.normalization import BatchNormalization
 from keras.regularizers import l2
-from keras.layers import GRU
+from keras.objectives import categorical_crossentropy
 
 from utils.models import MLC
-# from utils.selu import selu, dropout_selu
 
 
 def assemble(name, params):
@@ -19,140 +19,51 @@ def assemble(name, params):
         raise ValueError("Unknown name of the model: %s." % name)
 
 
-def assemble_adios(params):
-    """
-    Construct one of the ADIOS models. The general structure is the following:
-                                X-H-(Y0|H0)-H1-Y1,
-    where all the H-layers are optional and depend on whether they are
-    specified in the params dictionary.
-    """
-    # X
+class HISO(object):
+    def __init__(self, sentence_len, embed_size, vocab_size, Y0_dim, Y1_dim, mask_zero=True):
+        # input
+        inputs = tf.placeholder(tf.float32, [None, sentence_len], name='input')
+        # 1.base layers: embedding
+        embedding = Embedding(output_dim=embed_size, input_dim=vocab_size, input_length=sentence_len, mask_zero=mask_zero, name='embedding')(inputs)
+        # 2. semantic layers: Bidirectional GRU
+        Bi_GRU = Bidirectional(GRU(64), merge_mode='concat', name='Bi_GRU')(embedding)
 
-    input_shape = (params['X']['sequence_length'], params['X']['embedding_dim']
-                   ) if params['iter']['model_type'] == "CNN-static" else (
-                       params['X']['sequence_length'], )
-    X = Input(shape=input_shape, dtype='int32', name='X')
+        # 3. middle layer for predict Y0
+        Y0_preds = Dense(Y0_dim, activation='softmax', name='Y0_predictions')(Bi_GRU)
+        Y0 = tf.placeholder(tf.float32, [None, Y0_dim], name='Y0')
+        # 4. upper hidden layers
+        # Firstly, learn a hidden layer from Bi_GRU
+        # Secondly, consider Y0_preds as middle feature and combine it with hidden layer
+        hidden_layer_1 = Dense(64, name='hidden_layer_1')(Bi_GRU)
+        combine_layer = concatenate([Y0_preds, hidden_layer_1], axis=-1, name='combine_layer')
+        hidden_layer_2 = Dense(96, name='hidden_layer_2')(combine_layer)
+        # 5. layer for predict Y1
+        Y1_preds = Dense(Y1_dim, activation='softmax', name='Y1_predictions')(hidden_layer_2)
+        Y1 = tf.placeholer(tf.float32, [None, Y1_dim], name='Y1')
 
-    # embedding
-    # Static model do not have embedding layer
-    if params['iter']['model_type'] == "CNN-static":
-        embedding = BatchNormalization()(X)
-    elif 'embedding_dim' in params['X'] and params['X']['embedding_dim']:
-        embedding = Embedding(
-            output_dim=params['X']['embedding_dim'],
-            input_dim=params['X']['vocab_size'],
-            input_length=params['X']['sequence_length'],
-            name="embedding",
-            mask_zero=False)(X)
-        # embedding = BatchNormalization()(embedding)
-    else:
-        exit('embedding_dim param is not given!')
+        # Calculate loss
+        with tf.name_scope('loss'):
+            Y0_loss = tf.reduce_mean(categorical_crossentropy(Y0, Y0_preds), name='Y0_loss')
+            Y1_loss = tf.reduce_mean(categorical_crossentropy(Y1, Y1_preds), name='Y1_loss')
+            loss = tf.add(Y0_loss, Y1_loss, name='loss')
 
-    # expanding dimension
-    # embed_reshape = Reshape((params['X']['sequence_length'], params['X']['embedding_dim'], 1))(embedding)
+    def hamming_loss(self, data, preds):
+        # Compute the scores for each output separately
+        hl = {k: float((data[k] != preds[k]).sum(axis=1).mean()) for k in preds}
 
-    # multi-layer Conv and max-pooling
-    conv_layer_num = len(params['Conv1D'])
-    for i in range(1, conv_layer_num+1):
-        H_input = embedding if i == 1 else H
-        conv = Conv1D(
-            filters=params['Conv1D']['layer%s' % i]['filters'],
-            kernel_size=params['Conv1D']['layer%s' % i]['filter_size'],
-            padding=params['Conv1D']['layer%s' % i]['padding_mode'],
-            # activation='relu',
-            strides=1,
-            bias_regularizer=l2(0.01))(H_input)
-        conv_batch_norm = Activation('relu')(BatchNormalization()(conv))
-        # conv_batch_norm = selu(BatchNormalization()(conv))
-        pool_size = params['Conv1D']['layer%s' % i]['pooling_size']
-        conv_pooling = MaxPool1D(pool_size=pool_size)(conv_batch_norm)
-        # dropout
-        if 'dropout' in params['Conv1D']['layer%s' % i]:
-            H = Dropout(params['Conv1D']['layer%s' % i]['dropout'])(conv_pooling)
-            # H = dropout_selu(conv_pooling, params['Conv1D']['layer%s' % i]['dropout'])
+        # Concatenate the outputs and compute the overall score
+        targets_all = np.hstack([data[k] for k in preds])
+        preds_all = np.hstack([preds[k] for k in preds])
+        hl['all'] = float((targets_all != preds_all).sum(axis=1).mean())
 
-    # flatten
-    H = Flatten(name='H')(H)
-    # H = Dropout(0.3, name='H')(BatchNormalization()(GRU(128)(H)))
+    def rank_loss(self, labels, preds):
+        pass
 
-    # Y0 output
-    kwargs = params['Y0']['kwargs'] if 'kwargs' in params['Y0'] else {}
-    if 'W_regularizer' in kwargs:
-        kwargs['W_regularizer'] = l2(kwargs['W_regularizer'])
-    Y0 = Dense(
-        params['Y0']['dim'],
-        # activation='sigmoid',
-        name='Y0_active',
-        bias_regularizer=l2(0.01),
-        **kwargs)(H)
-    # batch_norm
-    if 'batch_norm' in params['Y0']:
-        Y0 = BatchNormalization(**params['Y0']['batch_norm'])(Y0)
-    Y0 = Activation('softmax')(Y0)
-    if 'activity_reg' in params['Y0']:
-        Y0 = ActivityRegularization(
-            name='Y0', **params['Y0']['activity_reg'])(Y0)
+    def one_error(self, labels, preds):
+        pass
 
-    # H0
-    if 'H0' in params:  # we have a composite layer (Y0|H0)
-        kwargs = params['H0']['kwargs'] if 'kwargs' in params['H0'] else {}
+    def Coverage(self, labels, preds):
+        pass
 
-        # ReLu
-        H0 = Dense(
-            params['H0']['dim'],
-            # activation='relu',
-            bias_regularizer=l2(0.01),
-            **kwargs)(H)
-        # batch_norm
-        if 'batch_norm' in params['H0'] and params['H0']['batch_norm']:
-            H0 = BatchNormalization(
-                name='H0_batchNorm', **params['H0']['batch_norm'])(H0)
-        H0 = Activation('relu')(H0)
-        # dropout
-        if 'dropout' in params['H0']:
-            H0 = Dropout(params['H0']['dropout'], name='H0_dropout')(H0)
-        Y0_H0 = concatenate([Y0, H0])
-        # Y0_H0 = H0
-    else:
-        Y0_H0 = Y0
-
-    # H1
-    if 'H1' in params:  # there is a hidden layer between Y0 and Y1
-        kwargs = params['H1']['kwargs'] if 'kwargs' in params['H1'] else {}
-
-        H1 = Dense(
-            params['H1']['dim'],
-            # activation='relu',
-            bias_regularizer=l2(0.01),
-            **kwargs)(Y0_H0)
-        # batch_norm
-        if 'batch_norm' in params['H1'] and params['H1']['batch_norm']:
-            H1 = BatchNormalization(
-                name='H1_batch_norm', **params['H1']['batch_norm'])(H1)
-        H1 = Activation('relu')(H1)
-        # dropout
-        if 'dropout' in params['H1']:
-            H1 = Dropout(params['H1']['dropout'], name='H1_dropout')(H1)
-    else:
-        H1 = Y0_H0
-
-    # Y1
-    kwargs = params['Y1']['kwargs'] if 'kwargs' in params['Y1'] else {}
-    if 'W_regularizer' in kwargs:
-        kwargs['W_regularizer'] = l2(kwargs['W_regularizer'])
-    Y1 = Dense(
-        params['Y1']['dim'],
-        # activation='sigmoid',
-        name='Y1_activation',
-        bias_regularizer=l2(0.01),
-        **kwargs)(H1)
-    # batch_norm
-    if 'batch_norm' in params['Y1']:
-        Y1 = BatchNormalization(**params['Y1']['batch_norm'])(Y1)
-    Y1 = Activation('softmax')(Y1)
-
-    if 'activity_reg' in params['Y1']:
-        Y1 = ActivityRegularization(
-            name='Y1', **params['Y1']['activity_reg'])(Y1)
-
-    return MLC(inputs=X, outputs=[Y0, Y1])
+    def average_precision(self, labels, preds):
+        pass
