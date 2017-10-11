7ae05873e6d6d8af318232dcf52cb9f0184d5c47
KillersDeath <KillersDeath@users.noreply.github.com>
2017-10-11 14:31:41 +0800

    test ok for model

---
+++ hiso/train.py
@@ -0,0 +1,121 @@
+# -*- coding:utf-8 -*-
+"""
+@version: 1.0
+@author: kevin
+@license: Apache Licence
+@contact: liujiezhang@bupt.edu.cn
+@site:
+@software: PyCharm Community Edition
+@file: adios_train.py
+@time: 17/05/03 17:39
+"""
+import tensorflow as tf
+from keras import backend as K
+from utils.assemble import HISO
+from utils.data_helper import build_data_cv
+
+
+def padding_sentence(hmlObjectList, sequence_len):
+    '''
+    padding_sentence
+    '''
+    print('start padding sentence...')
+    pad_data = [('<s>', '<s>')]
+
+    def padding(hml):
+        if hml.sentence_len <= sequence_len:
+            hml.content += pad_data * (sequence_len - hml.sentence_len)
+        else:
+            hml.content = hml.content[:sequence_len]
+        return hml
+
+    return list(map(padding, hmlObjectList))
+
+
+def word2index(hmlObjectList, vocab_word2index):
+    '''
+    transform word to index
+    '''
+    print('start transform word to index...')
+
+    def transform(hml):
+        wds = [wd for wd, _ in hml.content]
+        hml.vec = [vocab_word2index[wd] for wd in wds]
+        return hml
+
+    return list(map(transform, hmlObjectList))
+
+
+def do_eval(sess, model, eval_data, batch_size):
+    number_example = len(eval_data)
+    eval_loss, eval_acc, eval_counter = 0., 0., 0
+    for start, end in zip(
+            range(0, number_example, batch_size),
+            range(batch_size, number_example, batch_size)):
+        curr_eval_loss = sess.run(
+            model.loss,
+            feed_dict={
+                model.inputs: [hml.vec for hml in eval_data],
+                model.Y0: [hml.top_label for hml in eval_data],
+                model.Y1: [hml.bottom_label for hml in eval_data],
+            })
+
+        eval_loss += curr_eval_loss
+        eval_counter += 1
+
+        return eval_loss / eval_counter
+
+
+if __name__ == '__main__':
+    datas, vocab = build_data_cv('../docs/HML_JD_ALL.new.dat', cv=5)
+    # bulid vocab
+    vocab_wds = ['<s>'] + list(vocab.keys())
+    vocab_word2index = {wd: i for i, wd in enumerate(vocab_wds)}
+    vocab_index2word = {i: wd for wd, i in vocab_word2index.items()}
+    # split test and train
+    test_datas = filter(lambda data: data.cv_n == 1, datas)
+    train_datas = filter(lambda data: data.cv_n != 1, datas)
+    # process data
+    # 1. padding sentence
+    # 2. tranform to word index
+    test_datas = padding_sentence(test_datas, sequence_len=100)
+    train_datas = padding_sentence(train_datas, sequence_len=100)
+
+    test_datas = word2index(test_datas, vocab_word2index)
+    train_datas = word2index(train_datas, vocab_word2index)
+    print(train_datas[0])
+
+    print('train dataset: {}'.format(len(train_datas)))
+    print('test dataset: {}'.format(len(test_datas)))
+
+    number_of_training_data = len(train_datas)
+    batch_size = 32
+    # build model
+    with tf.Session() as sess:
+        K.set_session(sess)
+        hiso = HISO(
+            sentence_len=100,
+            Y0_dim=3,
+            Y1_dim=6,
+            vocab_size=len(vocab_wds),
+            embed_size=100)
+        init_op = tf.global_variables_initializer()
+        sess.run(init_op)
+        for epoch in range(3):
+            for start, end in zip(
+                    range(0, number_of_training_data, batch_size),
+                    range(batch_size, number_of_training_data, batch_size)):
+
+                inputs = [hml.vec for hml in train_datas[start:end]]
+                Y0 = [hml.top_label for hml in train_datas[start:end]]
+                Y1 = [hml.bottom_label for hml in train_datas[start:end]]
+
+                trn_loss, _ = sess.run(
+                    [hiso.loss, hiso.train_op],
+                    feed_dict={hiso.inputs: inputs,
+                               hiso.Y0: Y0,
+                               hiso.Y1: Y1})
+                if end / batch_size == 10:
+                    tst_loss = do_eval(sess, hiso, test_datas, batch_size)
+                    print('epoch: {}, train loss: {}, test loss: {}'.format(
+                        epoch, trn_loss, tst_loss))
--- hiso/utils/assemble.py
+++ hiso/utils/assemble.py
@@ -1,44 +1,69 @@
 """
 Utility functions for constructing MLC models.
 """
-import tensorflow as tf
 import numpy as np
-from keras.layers import Embedding, Dense, concatenate, GRU
+import tensorflow as tf
+from keras.layers import GRU, Dense, Embedding, concatenate
 from keras.layers.wrappers import Bidirectional
 from keras.objectives import categorical_crossentropy
 
 
 class HISO(object):
-    def __init__(self, sentence_len, embed_size, vocab_size, Y0_dim, Y1_dim, mask_zero=True):
+    def __init__(self,
+                 sentence_len,
+                 embed_size,
+                 vocab_size,
+                 Y0_dim,
+                 Y1_dim,
+                 mask_zero=True):
         # input
-        inputs = tf.placeholder(tf.float32, [None, sentence_len], name='input')
+        self.inputs = tf.placeholder(
+            tf.float32, [None, sentence_len], name='input')
         # 1.base layers: embedding
-        embedding = Embedding(output_dim=embed_size, input_dim=vocab_size, input_length=sentence_len, mask_zero=mask_zero, name='embedding')(inputs)
+        embedding = Embedding(
+            output_dim=embed_size,
+            input_dim=vocab_size,
+            input_length=sentence_len,
+            mask_zero=mask_zero,
+            name='embedding')(self.inputs)
         # 2. semantic layers: Bidirectional GRU
-        Bi_GRU = Bidirectional(GRU(64), merge_mode='concat', name='Bi_GRU')(embedding)
+        Bi_GRU = Bidirectional(
+            GRU(64), merge_mode='concat', name='Bi_GRU')(embedding)
 
         # 3. middle layer for predict Y0
-        Y0_preds = Dense(Y0_dim, activation='softmax', name='Y0_predictions')(Bi_GRU)
-        Y0 = tf.placeholder(tf.float32, [None, Y0_dim], name='Y0')
+        Y0_preds = Dense(
+            Y0_dim, activation='softmax', name='Y0_predictions')(Bi_GRU)
+        self.Y0 = tf.placeholder(tf.float32, [None, Y0_dim], name='Y0')
         # 4. upper hidden layers
         # Firstly, learn a hidden layer from Bi_GRU
         # Secondly, consider Y0_preds as middle feature and combine it with hidden layer
         hidden_layer_1 = Dense(64, name='hidden_layer_1')(Bi_GRU)
-        combine_layer = concatenate([Y0_preds, hidden_layer_1], axis=-1, name='combine_layer')
+        combine_layer = concatenate(
+            [Y0_preds, hidden_layer_1], axis=-1, name='combine_layer')
         hidden_layer_2 = Dense(96, name='hidden_layer_2')(combine_layer)
         # 5. layer for predict Y1
-        Y1_preds = Dense(Y1_dim, activation='softmax', name='Y1_predictions')(hidden_layer_2)
-        Y1 = tf.placeholer(tf.float32, [None, Y1_dim], name='Y1')
+        Y1_preds = Dense(
+            Y1_dim, activation='softmax',
+            name='Y1_predictions')(hidden_layer_2)
+        self.Y1 = tf.placeholder(tf.float32, [None, Y1_dim], name='Y1')
 
         # Calculate loss
         with tf.name_scope('loss'):
-            Y0_loss = tf.reduce_mean(categorical_crossentropy(Y0, Y0_preds), name='Y0_loss')
-            Y1_loss = tf.reduce_mean(categorical_crossentropy(Y1, Y1_preds), name='Y1_loss')
+            Y0_loss = tf.reduce_mean(
+                categorical_crossentropy(self.Y0, Y0_preds), name='Y0_loss')
+            Y1_loss = tf.reduce_mean(
+                categorical_crossentropy(self.Y1, Y1_preds), name='Y1_loss')
             self.loss = tf.add_n([Y0_loss, Y1_loss], name='loss')
+
+        self.train_op = tf.train.GradientDescentOptimizer(0.5).minimize(
+            self.loss)
 
     def hamming_loss(self, data, preds):
         # Compute the scores for each output separately
-        hl = {k: float((data[k] != preds[k]).sum(axis=1).mean()) for k in preds}
+        hl = {
+            k: float((data[k] != preds[k]).sum(axis=1).mean())
+            for k in preds
+        }
 
         # Concatenate the outputs and compute the overall score
         targets_all = np.hstack([data[k] for k in preds])
