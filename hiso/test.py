a74dfa874562d7842926079777c131a7a60849b2
KillersDeath <KillersDeath@users.noreply.github.com>
2018-01-23 11:10:00 +0800

    load lookup

--- hiso/test.py
+++ hiso/test.py
@@ -1,7 +1,20 @@
 # coding:utf8
 import pandas as pd
 import numpy as np
+import pickle
+import torch
+from config import params
+from sswe import SSWE
+
 if __name__ == '__main__':
+    
+    model = SSWE(params)
+    model.load_state_dict(torch.load('../docs/model/sswe_01-22-10:03'))
+    lookup = sswe.lookup.weight.data.cpu().numpy()
+    pickle.dump(lookup,open('../docs/model/lookup_01-22-10:03','wb'))
+    exit()
+
+
     df = pd.read_pickle('../docs/data/HML_JD_ALL.new.dat')
     oj = (df['Event']>0) | (df['Agent']>0)|(df['Object']>0) 
     fj = (df['Satisfaction']>0)|(df['Disappointment']>0)|(df['Admiration']>0)|(df['Reproach']>0)|(df['Like']>0)|(df['Dislike']>0)
