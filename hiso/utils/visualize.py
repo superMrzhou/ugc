9f41169b92377a6e28b14d0c26181e84e4d93431
Kevin <liujiezhang@bupt.edu.cn>
2018-04-07 12:41:53 +0800

    rm visdom support

--- hiso/utils/visualize.py
+++ hiso/utils/visualize.py
@@ -1,4 +1,4 @@
-import visdom
+# import visdom
 import torch
 import time
 import torchvision as tv
@@ -10,7 +10,7 @@ class Visualizer():
     封装了visdom，tensorboard_logger, 更方便记录loss
     '''
     def __init__(self, env='default',log_dir='runs/BiGRU', **kwargs):
-        self.vis = visdom.Visdom(env=env, **kwargs)
+#         self.vis = visdom.Visdom(env=env, **kwargs)
         self.tenbd = Logger(log_dir, flush_secs=2)
 
         # 记录数据的横向坐标{'img':2, 'loss':12}
@@ -18,24 +18,24 @@ class Visualizer():
         # 记录一些log信息
         self.log_text = ''
         
-    def reinit(self, env='default', **kwargs):
-        '''
-        更改visdom的配置
-        '''
-        self.vis =  visdom.Visdom(env=env, **kwargs)
+#     def reinit(self, env='default', **kwargs):
+#         '''
+#         更改visdom的配置
+#         '''
+#         self.vis =  visdom.Visdom(env=env, **kwargs)
 
-        return vis
+#         return vis
 
     def plot(self, name, y):
         '''
         self.plot('loss',0.23)
         '''
         x = self.index.get(name, 0)
-        self.vis.line(Y=np.array([y]),
-                X=np.array([x]),
-                win=name,
-                opts=dict(title=name),
-                update=None if x==0 else 'append')
+#         self.vis.line(Y=np.array([y]),
+#                 X=np.array([x]),
+#                 win=name,
+#                 opts=dict(title=name),
+#                 update=None if x==0 else 'append')
         self.tenbd.log_value(name,y,x)
         
         self.index[name] = x + 1
@@ -47,17 +47,17 @@ class Visualizer():
         for k, v in data.iteritems():
             self.plot(k,v)
     
-    def  log(self,info,win='log_text'):
-        '''
-        self.log({'loss':1, 'lr':0.001})
-        '''
-        self.log_text += (
-                '[{time}] {info} <br>'.format(
-                    time=time.strftime('%m%d_%H%M%S'), 
-                    info=info
-                    )
-                )
-        self.vis.text(self.log_text, win='log_text')
+#     def  log(self,info,win='log_text'):
+#         '''
+#         self.log({'loss':1, 'lr':0.001})
+#         '''
+#         self.log_text += (
+#                 '[{time}] {info} <br>'.format(
+#                     time=time.strftime('%m%d_%H%M%S'), 
+#                     info=info
+#                     )
+#                 )
+#         self.vis.text(self.log_text, win='log_text')
 
-    def __getattr__(self,name):
-        return getattr(self.vis, name)
+#     def __getattr__(self,name):
+#         return getattr(self.vis, name)
